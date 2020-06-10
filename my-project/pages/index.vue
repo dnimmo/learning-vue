934ae5ad6134ae529c3747532d5d6a359644a9e4
David Nimmo <dnimmo@gmail.com>
2020-06-10 13:25:18 +0100

    added Vue template for single "destination"

---
+++ my-project/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+    <div>
+      <upcoming-trips />
+    </div>
+  </section>
+</template>
+
+<script>
+import UpcomingTrips from '~/components/UpcomingTrips.vue'
+
+export default {
+  components: {
+    UpcomingTrips
+  }
+}
+</script>
+
+
