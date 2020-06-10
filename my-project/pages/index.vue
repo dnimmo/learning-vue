0290ddafa955d99612eb977339f6356af11efe8f
David Nimmo <dnimmo@gmail.com>
2020-06-10 15:56:18 +0100

    updated to present entire list of locations

--- my-project/pages/index.vue
+++ my-project/pages/index.vue
@@ -1,17 +1,48 @@
 <template>
   <section class="container">
-    <div>
-      <upcoming-trips />
+    <div class="location-contain">
+    <div class="locations" >
+      <ul>
+        <li v-for="(trip, i) in trips" :key="i"><trip-card :location="trip"/></li>
+      </ul>
     </div>
+  </div>
   </section>
 </template>
 
 <script>
-import UpcomingTrips from '~/components/UpcomingTrips.vue'
+
+import TripCard from '~/components/TripCard.vue'
 
 export default {
   components: {
-    UpcomingTrips
+    TripCard
+  },
+  data() {
+    return {
+      trips: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+         {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+        },
+         {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+      ]
+    }
   }
 }
 </script>
